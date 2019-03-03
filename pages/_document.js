10056b4551a99ef9d938cd857593c167b398dede
Unknown <unknown@example.com>
2019-03-03 22:12:45 +1100

    material ui config added

--- pages/_document.js
+++ pages/_document.js
@@ -1,37 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import flush from 'styled-jsx/server'
 
-export default class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const sheet = new ServerStyleSheet()
-        const originalRenderPage = ctx.renderPage
-
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: App => props =>
-                        sheet.collectStyles(<App {...props} />),
-                })
-
-            const initialProps = await Document.getInitialProps(ctx)
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
-            }
-        } finally {
-            sheet.seal()
-        }
-    }
-
+class MyDocument extends Document {
     render() {
+        const { pageContext } = this.props
+
         return (
-            <html>
-                <Head>{this.props.styleTags}</Head>
+            <html lang="en" dir="ltr">
+                <Head>
+                    <meta charSet="utf-8" />
+                    {/* Use minimum-scale=1 to enable GPU rasterization */}
+                    <meta
+                        name="viewport"
+                        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+                    />
+                    {/* PWA primary color */}
+                    <meta
+                        name="theme-color"
+                        content={
+                            pageContext
+                                ? pageContext.theme.palette.primary.main
+                                : null
+                        }
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+                    />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
@@ -41,28 +39,43 @@ export default class MyDocument extends Document {
     }
 }
 
-// import Document, { Head, Main, NextScript } from 'next/document'
-// import { ServerStyleSheet } from 'styled-components'
+MyDocument.getInitialProps = ctx => {
+    // Render app and page and get the context of the page with collected side effects.
+    let pageContext
+    const page = ctx.renderPage(Component => {
+        const WrappedComponent = props => {
+            pageContext = props.pageContext
+            return <Component {...props} />
+        }
 
-// export default class MyDocument extends Document {
-//     static getInitialProps({ renderPage }) {
-//         const sheet = new ServerStyleSheet()
-//         const page = renderPage(App => props =>
-//             sheet.collectStyles(<App {...props} />)
-//         )
-//         const styleTags = sheet.getStyleElement()
-//         return { ...page, styleTags }
-//     }
+        WrappedComponent.propTypes = {
+            pageContext: PropTypes.object.isRequired,
+        }
 
-//     render() {
-//         return (
-//             <html>
-//                 <Head>{this.props.styleTags}</Head>
-//                 <body>
-//                     <Main />
-//                     <NextScript />
-//                 </body>
-//             </html>
-//         )
-//     }
-// }
+        return WrappedComponent
+    })
+
+    let css
+    // It might be undefined, e.g. after an error.
+    if (pageContext) {
+        css = pageContext.sheetsRegistry.toString()
+    }
+
+    return {
+        ...page,
+        pageContext,
+        // Styles fragment is rendered after the app and page rendering finish.
+        styles: (
+            <React.Fragment>
+                <style
+                    id="jss-server-side"
+                    // eslint-disable-next-line react/no-danger
+                    dangerouslySetInnerHTML={{ __html: css }}
+                />
+                {flush() || null}
+            </React.Fragment>
+        ),
+    }
+}
+
+export default MyDocument
